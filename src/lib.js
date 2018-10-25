4811baead60416f485c8f44459923d80cd68f7c9
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2018-10-25 15:25:56 +0530

    fiboSeriesGenerator added

--- src/lib.js
+++ src/lib.js
@@ -24,27 +24,88 @@ const makeCounterFromZero = function(){
   return counterFromZero;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//--------------
 
 const makeDeltaTracker = undefined;
-const makeFiboGenerator = undefined;
+/*
+function(currOld){ 
+
+    const updateOld = function(){
+    return obj.new;
+  }
+
+  const trackDel = function(currDelta){
+
+    let obj = {old: 0, delta: 0, new: 0};
+    obj.old = currOld ;
+
+    if(currDelta != undefined){
+      obj.delta = currDelta;
+    }
+
+    let valueOfNew = obj.delta + obj.old;
+    obj.new = valueOfNew;
+    obj.old = obj.new;
+
+    return obj; 
+  }
+
+  return trackDel;
+}
+
+*/
+
+//----------------------
+//currently its full with ugly code, and stupid logics. plz take precaution.
+
+const makeFiboGenerator = function(num1,num2){
+  let firstFibo = -1;
+  let secondFibo = 1;
+  let count = 0;
+
+  if(num1 != undefined){
+    firstFibo = -num1; 
+    secondFibo = num1;  }
+
+  if(num2 != undefined){
+    secondFibo = num2 - num1;
+    firstFibo = num1 - secondFibo;
+  }
+
+  const getNextFibo = function(){
+   let result = firstFibo + secondFibo;
+
+   const updateFibo = function(){
+     firstFibo = secondFibo;
+     secondFibo = result;
+   }
+        
+    updateFibo();
+
+    return result;
+  }
+
+  return getNextFibo;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const makeCycler = undefined;
 const curry = undefined;
 const compose = undefined;
